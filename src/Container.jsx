99d0fd22dbcdb5784aa0ec927efc29e8ddf648df
toobler <anittasunny39@gmail.com>
2022-08-22 19:29:25 +0530

    done

--- src/Container.jsx
+++ src/Container.jsx
@@ -7,16 +7,16 @@ import { BestServices } from "./components/main/bestServices/BestServices";
 import { Service1 } from "./components/main/service1/Service1";
 import { Blog } from "./components/main/blog/Blog";
 import { Section1 } from "./components/main/section1/section1";
+import { About } from "./components/main/about/About";
 
 function Container() {
   
   return (
     <>
       <Header />
-
       <Hero />
-
       <Section1/>
+      <About/>
       <Service1/>
       <BestServices />
       <Blog />
---
+++ src/components/main/about/About.jsx
@@ -0,0 +1,28 @@
+import "./Styles.css";
+import "./ResponsiveAbout.css";
+import AboutPic from "../../../assets/image5.svg";
+
+export const About = () => {
+    return (
+        <section className="about">
+               <h1>About</h1>
+                        <div className="aboutWrapper">
+
+                <div className="leftAbout">
+                    <img src={AboutPic} alt="lady" />
+
+                </div>
+                <div className="rightAbout">
+                    <div className="vert-head">
+                      we are doing some high-end stuff for a lot of clients
+                    </div>
+                    <div>
+                        jkkkkkkkkkkhhhhhhhhhhhhhhhhhhh
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+    );
+};
